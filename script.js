d4915754f7074b0428066fb3607028023dc3d8ca
Tung Do <dothanhtung132@gmail.com>
2025-01-30 10:36:42 +1030

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,8 @@
 // @version      0.0.1
 // @match        *://*/*
 // @grant        none
-// @run-at       document-end
 // ==/UserScript==
 (function() {
     'use strict';
-    let removeEls = () => {
-        document.querySelectorAll('a[href*="shopee"][target="_blank"], a[href*="lazada"][target="_blank"]').forEach(item => item && item.remove());
-    }
-    removeEls();
-    setTimeout(function() {
-        removeEls();
-    }, 200);
+    document.querySelectorAll('a[href*="shopee"][target="_blank"], a[href*="lazada"][target="_blank"]').forEach(item => item && item.remove());
 })();
